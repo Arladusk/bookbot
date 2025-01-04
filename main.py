d08ef2b8831b7961bf27bed4c29c4ae0791f8b98
Arladusk <Simonchristensen@live.se>
2025-01-04 21:23:48 +0100

    add word counter

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+def word_counter(sentence: str) -> int:
+    words = sentence.split()
+    print(f"Number of words:{len(words)}")
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
+        word_counter(file_contents)
 
 main()
