5c9f0fcf83112409cc6b6c0a4da04a2fcaac7b09
Arladusk <Simonchristensen@live.se>
2025-01-05 19:14:52 +0100

    rewrote word_counter to return a list

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-def word_counter(sentence: str) -> int:
+def word_counter(sentence: str) -> list:
     words = sentence.split()
     print(f"Number of words:{len(words)}")
+    return words
 
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        word_counter(file_contents)
-
+        words = word_counter(file_contents)
+        character_counter(words)
 main()
